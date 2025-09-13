7f47c8d682fa6956e475d0fe006d5713c6ae737c
Nicholas Tsimerekis <ntsimerekis@protonmail.com>
2025-09-12 20:58:19 -0700

    Sorry, accidentally used isbn instead of publisher

--- src/com/tsimerekis/aggregation/Driver.java
+++ src/com/tsimerekis/aggregation/Driver.java
@@ -4,7 +4,7 @@ public class Driver {
 
 	public static void main(String[] args) {
 		final Instructor instructor = new Instructor("Nima", "Davarpanah", "8-49");
-		final Textbook textbook = new Textbook("Robert", "Marin", "Clean Code: A Handbook of Agile Software Craftsmanship","978-0132350884");
+		final Textbook textbook = new Textbook("Robert", "Marin", "Clean Code: A Handbook of Agile Software Craftsmanship","Appress");
 		
 		final Course course = new Course("Advanced Software Engineering", instructor, textbook);
 		
@@ -12,7 +12,7 @@ public class Driver {
 		System.out.println("Now with a second textbook and instructor");
 		
 		course.addInstructor(new Instructor("Nicholas", "Tsimerekis", "8-50"));
-		course.addTextbook(new Textbook("Brian", "Kerningham", "The C Programming Language", "9780131101630"));
+		course.addTextbook(new Textbook("Brian", "Kerningham", "The C Programming Language", "Prentice Hall"));
 		
 		System.out.println(course);
 	}
